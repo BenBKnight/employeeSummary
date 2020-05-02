d421097212afa794901e274af8a9f4affd34ba4a
Ben K <ben.knight.brazil@hotmail.com>
2020-05-01 19:57:18 -0600

    Employee.js passed all tests

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,54 +1,22 @@
 // TODO: Write code to define and export the Employee class
 const inquirer = require("inquirer")
 
-class Employee {
-    constructor (name, id, email){
-        this.name = name;
-        this.id = id;
-        this.email = email;
-    }
-}
 
-
-
-const questions = [
-    {
-        type: "input",
-        name: "name",
-        message: "Name of Employee",
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "ID of the Employee",
-    }, {
-        type: "input",
-        name: "email",
-        message: "Employee's email",
-    }
-];
-inquirer.prompt(questions).then(answers => {
-    console.log(answers);
-    const newEmployee = new Employee(answers.name, answers.id, answers.email)
-    console.log(newEmployee.name)
-    console.log(newEmployee.id)
-    console.log(newEmployee.email)
-})
-// Name
-
-// ID
-
-// Email
-
-// getName()
-
-// getId()
-
-// getEmail()
-
-// getRole() // Returns 'Employee'
-// 
-// 
-// 
-// 
-// 
+function Employee(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.getName = function(){
+        return name;
+    };
+    this.getId = function(){
+        return id;
+    };
+    this.getEmail = function(){
+        return email;
+    };
+    this.getRole = function(){
+        return "Employee";
+    };
+};
+module.exports = Employee
